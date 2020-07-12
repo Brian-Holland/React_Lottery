a626e4e88e9432c791ca0263017c46f4a11389a5
Brian <brian.holland.pr@gmail.com>
2020-07-11 21:52:13 -0700

    initial commit

---
+++ src/Lottery.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Ball from "./Ball";
+import "./Lottery.css";
+
+class Lottery extends Component {
+    static defaultProps = {
+        title: "Lotto",
+        maxBalls: 6,
+        maxNum: 40,
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = { nums: Array.from({ length: this.props.maxBalls }) };
+        this.generate = this.generate.bind(this);
+    }
+
+    generate() {
+        //map over nums array and generate number from 1-maxNum and set that to the new nyums state
+        this.setState(curState => ({
+            nums: curState.nums.map(
+                n => Math.floor(Math.random() * this.props.maxNum) + 1
+            ),
+        }));
+    }
+
+    render() {
+        return (
+            <section className="Lottery">
+                <h1>{this.props.title}</h1>
+                <div>
+                    {this.state.nums.map(n => (
+                        <Ball num={n} />
+                    ))}
+                </div>
+                <button onClick={this.generate}>Generate</button>
+            </section>
+        );
+    }
+}
+
+export default Lottery;
